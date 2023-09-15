180dedbd5f19bd755d43835604a2055847467423
Adarsh Kumar <60352729+AdarshIITDH@users.noreply.github.com>
2023-09-15 09:38:18 +0530

    Update adarsh.sh

--- adarsh.sh
+++ adarsh.sh
@@ -1,2 +1,6 @@
-sudo docker run --name=adarsh007 -d --rm --gpus all --tmpfs /run --tmpfs /run/lock --tmpfs /tmp --cgroupns=host --cap-add SYS_BOOT --cap-add SYS_ADMIN \ -v /sys/fs/cgroup:/sys/fs/cgroup -p 5901:5901 -p 6901:6901 adarsh321/adarsh:01
-
+sudo docker run --name=adarsh007 --gpus all -d --rm \
+  --tmpfs /run --tmpfs /run/lock --tmpfs /tmp \
+  --cgroupns=host --cap-add SYS_BOOT --cap-add SYS_ADMIN \
+  -v /sys/fs/cgroup:/sys/fs/cgroup \
+  -p 5901:5901 -p 6901:6901 \
+  adarsh321/adarsh:01
